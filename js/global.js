7a5537bda675f61ca692eadbccb64f6f69d684c2
Daniel Buchner <danieljb2@gmail.com>
2021-05-10 13:20:46 -0700

    Push new Issues/specs tab

--- js/global.js
+++ js/global.js
@@ -1,22 +1,57 @@
 
+import '/js/web-components/tab-panels.js';
+
+const md = window.markdownit({
+  linkify: true
+});
+
+// FETCH ISSUES
+
 async function getRemainingIssues(){
   var cache = JSON.parse(localStorage.remainingIssues || '{ "issues": [] }');
   var time = cache.time ? new Date(cache.time) : new Date();
   if (location.hostname === 'localhost' || !cache.time || (new Date() > time.setHours(time.getHours() + 1))) {
-    let issues = await fetch(`https://api.github.com/repos/decentralized-identity/waci-presentation-exchange/issues?per_page=100&labels=v0.1`, {
+    let issueList = await fetch(`https://api.github.com/repos/decentralized-identity/waci-presentation-exchange/issues?per_page=100&labels=v0.1`, {
       headers: { Accept: 'application/vnd.github.mercy-preview+json' }
     }).then(response => response.json())
       .catch(e => console.log(e));
     localStorage.remainingIssues = JSON.stringify(cache = {
-      issues: issues || [],
+      issues: issueList || [],
       time: new Date()
     });
   }
   goal_issue_count.textContent = cache.issues.length;
+  issues.innerHTML = cache.issues.reduce((html, issue) => {
+    return html += `
+      <li>
+        <h3>${issue.title}</h3>
+        <p>${md.render(issue.body)}</p>
+      </li>
+    `;
+  }, '')
   return cache.issues;
 }
 
 getRemainingIssues();
+
+// LISTEN FOR HASHCHANGE
+
+function onHashchange(){
+  switch (location.hash) {
+    case '#issues':
+      tabs.setAttribute('selected-index', 1);
+  }
+}
+
+window.addEventListener('hashchange', e => onHashchange());
+
+DOM.delegateEvent('click', '[show-issues]', (e) => {
+  tabs.setAttribute('selected-index', 1);
+}, { passive: true });
+
+onHashchange();
+
+
 
 let sounds;
 orcaegg.addEventListener('click', e => {
